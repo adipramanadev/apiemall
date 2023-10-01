983eb364b5967f00b7975b32c35e7260bb77dcee
adipramanadev <herryprasetyo.dev@gmail.com>
2023-10-01 15:42:43 +0700

    model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\SubCategory;
 
 class Category extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    /**
+     * Get all of the comments for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function subCategories()
+    {
+        return $this->hasMany(SubCategory::class);
+    }
 }
