adbd656f2419ee864e3199881f25f1bf7ab2df50
adipramanadev <herryprasetyo.dev@gmail.com>
2023-10-06 10:26:16 +0700

    tambahan

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,6 +13,8 @@ class CategoryController extends Controller
     public function index()
     {
         //
+        $category = Category::all();
+        return view('admin.category.index', compact('category'));
     }
 
     /**
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div class="footer-left">
+      Copyright &copy; @php echo date('Y'); @endphp <div class="bullet"></div> Design By <a href="#">Herry Prasetyo</a>
+    </div>
+  </footer>
